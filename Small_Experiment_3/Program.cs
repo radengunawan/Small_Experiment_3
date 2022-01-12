d59012f2dca3af479dddf712d668b0f0bfdd256f
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-12 15:05:15 +0800

    Add project files.

---
+++ Small_Experiment_3/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+List<Author> a1 = new List<Author> ();
+List <int> IDs = new List<int> ();
+List <string> names = new List<string> { "Dayat", "Saul", "Kasmin", "Erwin", "Qomarr"};
+
+for (int i = 0; i < names.Count; i++) {
+
+    IDs.Add(i);
+}
+
+for (int i = 0; i < names.Count; i++)
+{
+    a1.Add (new Author { ID = IDs[i].ToString(), Name = names[i] });
+}
+
+Console.WriteLine("Before removal");
+
+foreach (Author item in a1)
+{
+    Console.WriteLine (item.Name+" -- "+item.ID);
+}
+
+foreach (Author item in a1)
+{
+    if (item.ID == "1")
+    {
+        a1.Remove (item);
+        break;
+
+    }
+}
+
+Console.WriteLine("AFTER removal");
+
+foreach (Author item in a1)
+{
+    Console.WriteLine(item.Name + " -- " + item.ID);
+}
+
+
+
+
+public class Author
+{
+    public string Name { get; set; }
+    public string ID { get; set; }
+}
