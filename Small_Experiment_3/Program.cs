823595e80884294c4ad6708e33c6e8096fcf46e9
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-13 09:35:34 +0800

    using .getType()

--- Small_Experiment_3/Program.cs
+++ Small_Experiment_3/Program.cs
@@ -2,43 +2,51 @@
 //Console.WriteLine("Hello, World!");
 
 
-List<Author> a1 = new List<Author> ();
-List <int> IDs = new List<int> ();
-List <string> names = new List<string> { "Dayat", "Saul", "Kasmin", "Erwin", "Qomarr"};
+List<Author> a1 = new List<Author>();
+List<int> IDs = new List<int>();
+List<string> names = new List<string> { "Dayat", "Saul", "Kasmin", "Erwin", "Qomarr" };
 
-for (int i = 0; i < names.Count; i++) {
+//--------------GETTING TYPE of object
+//string a = "I wn candy";
+//var x = a.GetType;
 
-    IDs.Add(i);
+//Console.WriteLine(x.GetType);
+
+for (int i = 0; i < names.Count; i++)
+{
+
+IDs.Add(i);
 }
 
 for (int i = 0; i < names.Count; i++)
 {
-    a1.Add (new Author { ID = IDs[i].ToString(), Name = names[i] });
+a1.Add(new Author { ID = IDs[i].ToString(), Name = names[i] });
 }
 
 Console.WriteLine("Before removal\n");
 
 foreach (Author item in a1)
 {
-    Console.WriteLine (item.Name+" -- "+item.ID);
+    Console.WriteLine(item + " (" + item.GetType() + ")");
+    Console.WriteLine(item.Name + " (" + item.Name.GetType() + ")" + " -- " + item.ID + " (" + item.ID.GetType() + ")");
 }
 
 foreach (Author item in a1)
 {
-    if (item.ID == "1")
-    {
-        a1.Remove (item);
-        break;
-        //continue;
+if (item.ID == "1")
+{
+a1.Remove(item);
+break;
+//continue;
 
-    }
+}
 }
 
 Console.WriteLine("\nAFTER removal");
 
 foreach (Author item in a1)
 {
-    Console.WriteLine(item.Name + " -- " + item.ID);
+Console.WriteLine(item.Name + " ("+ item.Name.GetType()+ ")"+" -- " + item.ID + " (" + item.ID.GetType() + ")");
 }
 
 
@@ -49,3 +57,4 @@ public class Author
     public string Name { get; set; }
     public string ID { get; set; }
 }
+
